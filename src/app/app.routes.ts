1403c5e8e1d5a48498b92a0252b6c2e264ae6685
roberto <roberto.zuccaro@itsincom.it>
2025-01-29 11:36:12 +0100

    creation of subpages speakers and fix header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,14 +7,19 @@ import { PrenotazioneComponent } from './pages/prenotazione/prenotazione.compone
 import { AreaUtenteComponent } from './pages/area-utente/area-utente.component';
 import {RegisterComponent} from './pages/auth/register/register.component';
 import {LoginComponent} from './pages/auth/login/login.component';
+import {
+  RelatoreDetailComponent
+} from './pages/talk-relatori/relatore-detail-component/relatore-detail-component.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'eventi', component: EventiComponent },
-  { path: 'meeting', component: TalkRelatoriComponent },
+  { path: 'relatori', component: TalkRelatoriComponent },
+  { path: 'relatori/:id', component: RelatoreDetailComponent },
   { path: 'partner', component: PartnerComponent },
   { path: 'prenotazione', component: PrenotazioneComponent },
   { path: 'area-utente', component: AreaUtenteComponent },
   { path: 'auth/login', component: LoginComponent },
   { path: 'auth/register', component: RegisterComponent },
+
 ];
--- src/app/service/relatori.service.ts
+++ src/app/service/relatori.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 export interface Relatore {
+  userId: string;
+  id: string;
   name: string;
   surname: string;
   biography: string;
@@ -14,9 +16,20 @@ export interface Relatore {
 export class RelatoriService {
   private apiUrl = 'http://localhost:8080/speakers';
 
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient) {
+  }
+
 
   getRelatori(): Observable<Relatore[]> {
     return this.http.get<Relatore[]>(this.apiUrl);
   }
+
+  // Fetch a specific speaker by ID
+  getRelatoreById(id: string): Observable<Relatore> {
+    const url = this.apiUrl + '/' + id;
+    return this.http.get<Relatore>(url);
+  }
+
+
 }
