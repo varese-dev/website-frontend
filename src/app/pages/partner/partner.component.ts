8588c6a85b2cb0b41e3b4cedf0e7447c9817e11c
AlessandroAgostini <alealeago87@gmail.com>
2025-01-29 10:09:03 +0100

    modify partners 2

---
+++ src/app/pages/partner/partner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-partner',
+  standalone: true,
+  templateUrl: './partner.component.html',
+  styleUrls: ['./partner.component.css'],
+})
+export class PartnerComponent {
+  constructor() {}
+}
