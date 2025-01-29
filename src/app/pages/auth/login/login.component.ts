b70298c9ebfd4f818a1d310b5d5b4e74b1afa6b6
roberto <roberto.zuccaro@itsincom.it>
2025-01-29 15:35:31 +0100

    api related registration and login

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import {Router, RouterLink} from '@angular/router';
+import { AuthService, LoginData } from '../../../service/auth.service';
 import { FormsModule } from '@angular/forms';
-import { AuthService } from '../../../service/auth.service';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  imports: [FormsModule, RouterLink],
   styleUrls: ['./login.component.css'],
+  imports: [
+    FormsModule,
+    RouterLink,
+    NgIf
+  ]
 })
 export class LoginComponent {
   email: string = '';
@@ -15,36 +20,29 @@ export class LoginComponent {
   phone: string = '';
   errorMessage: string = '';
 
-  //constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   login(): void {
-    // Controlla se almeno email/telefono e password sono stati inseriti
     if ((!this.email && !this.phone) || !this.password) {
-      this.errorMessage = 'Email/Telefono e password sono obbligatori';
+      this.errorMessage = 'Inserisci email o telefono e password';
       return;
     }
 
-    // Determina quale campo utilizzare (email o phoneNumber)
-    const credentials = this.email ? { email: this.email } : { phone: this.phone };
+    const credentials: LoginData = {
+      email: this.email || undefined,
+      phone: this.phone || undefined,
+      password: this.password,
+    };
 
-    // Verifica che almeno uno tra email o phoneNumber sia presente
-    const credential = credentials.email || credentials.phone;
-    if (!credential) {
-      this.errorMessage = 'Inserisci un\'email o un numero di telefono valido.';
-      return;
-    }
-
-    /* Chiamata al servizio di login
-    this.authService.login(credential, this.password).subscribe({
+    this.authService.login(credentials).subscribe({
       next: (response) => {
-        this.errorMessage = '';
-        console.log(response);
-        this.router.navigate(['/home']);
+        console.log('Login riuscito:', response);
+        this.router.navigate(['/area-utente']);
       },
       error: (err) => {
-        this.errorMessage = 'Credenziali errate, riprova.';
-        console.error(err);
+        console.error('Errore di login:', err);
+        this.errorMessage = 'Credenziali errate o errore di connessione.';
       },
-    });*/
+    });
   }
 }
