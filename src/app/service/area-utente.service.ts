41b69dea6d7a0b2b4717506260f87147c60c8db0
Mattia Mossali <156786475+Muschiooo@users.noreply.github.com>
2025-02-04 17:37:30 +0100

    Merge pull request #7
    * fetch in user dashboard to get user data and user bookings
    
    * implemented booking cancellation endpoint
    
    * canceled bookings separated from the others
    
    * small css for user dashboard

--- src/app/service/area-utente.service.ts
+++ src/app/service/area-utente.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, map, switchMap } from 'rxjs/operators';
 
-export interface User {
+interface User {
   id: string;
   name: string;
+  surname: string;
+  email: string;
+  phone: string;
+}
+
+interface Booking {
+  bookingId: string;
+  title: string;
+  date: string;
+  status: string;
 }
 
 @Injectable({
@@ -13,16 +23,38 @@ export interface User {
 })
 export class AreaUtenteService {
   private apiUrl = 'http://localhost:8080/user';
+  private  bookingUrl = 'http://localhost:8080/bookings';
+
 
   constructor(private http: HttpClient) {}
 
-  /*getUserById(userId: string): Observable<User> {
-    const url = `${this.apiUrl}/${userId}`;
-    return this.http.get<User>(url, { withCredentials: true }).pipe(
-      catchError(error => {
-        console.error('Errore nel recupero dell\'utente:', error);
-        return throwError(() => new Error('Errore nel recupero dell\'utente'));
-      })
+  fetchUserId(): Observable<string> {
+    return this.http.get<{ userId: string }>(`${this.apiUrl}/session`, { withCredentials: true }).pipe(
+      map(response => response.userId)
     );
-  }*/
+  }
+
+  fetchUserData(): Observable<User> {
+    return this.fetchUserId().pipe(
+      switchMap(userId => this.http.get<User>(`${this.apiUrl}/${userId}`, { withCredentials: true })),
+      catchError(this.handleError)
+    );
+  }
+
+  fetchUserActiveBookings(): Observable<Booking[]> {
+    return this.http.get<Booking[]>(`${this.bookingUrl}/user/details`, { withCredentials: true }).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  cancelBooking(bookingId: string): Observable<void> {
+    return this.http.put<void>(`${this.bookingUrl}/${bookingId}/cancel`, null, { withCredentials: true }).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    console.error('Error fetching user data:', error);
+    return throwError('Errore durante il recupero dei dati utente. Riprova più tardi.');
+  }
 }
