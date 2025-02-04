7db018e3878f84cdd7432c83d5214940119b7b55
roberto <roberto.zuccaro@itsincom.it>
2025-02-04 17:49:24 +0100

    creation of admin page

---
+++ src/app/service/admin.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminService {
+  private apiUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {}
+
+  // Creazione evento
+  createEvent(eventData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/events`, eventData, { withCredentials: true });
+  }
+
+  // Creazione partner
+  createPartner(partnerData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/partners`, partnerData, { withCredentials: true });
+  }
+
+  // Creazione speaker
+  createSpeaker(speakerData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/speakers`, speakerData, { withCredentials: true });
+  }
+
+  // Creazione tag
+  createTag(tagData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/tags`, tagData, { withCredentials: true });
+  }
+
+  // Creazione talk
+  createTalk(talkData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/talks`, talkData, { withCredentials: true });
+  }
+}
