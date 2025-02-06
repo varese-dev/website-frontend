2a83452066a266af50020d2a8f8ab65da4e373cc
“Leonardo <sl.falese@gmail.com>
2025-02-06 17:44:22 +0100

    animation hero and hamburger header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -15,6 +15,7 @@ export class HeaderComponent implements AfterViewInit {
   @ViewChild('mainHeader', { static: true }) mainHeader!: ElementRef;
   isScrolled = false;
   isHomePage = false;
+  isMenuOpen = false;
 
   constructor(private router: Router, private authService: AuthService) {
     this.router.events.subscribe(event => {
@@ -27,6 +28,44 @@ export class HeaderComponent implements AfterViewInit {
   @HostListener('window:scroll', [])
   onScroll(): void {
     this.isScrolled = window.scrollY > 50;
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+
+    const navElement = document.querySelector('nav');
+
+    if (this.isMenuOpen) {
+      // Apertura con animazione
+      navElement!.style.display = 'flex';
+      navElement!.style.overflow = 'hidden';
+
+      gsap.fromTo(
+        navElement,
+        { height: 0, opacity: 0 },
+        {
+          height: 'auto',
+          opacity: 1,
+          duration: 1.5,
+          ease: 'power3.out',
+          onComplete: () => {
+            navElement!.style.overflow = 'visible';
+          },
+        }
+      );
+    } else {
+      // Animazione di chiusura più lenta per far sparire gradualmente
+      gsap.to(navElement, {
+        opacity: 0,
+        duration: 0.6,
+        ease: 'power1.out',
+        onComplete: () => {
+          navElement!.style.display = 'none';  // Nasconde dopo l'animazione
+          navElement!.style.height = '';       // Ripristina altezza
+          navElement!.style.opacity = '';      // Ripristina opacità
+        },
+      });
+    }
   }
 
   ngAfterViewInit() {
